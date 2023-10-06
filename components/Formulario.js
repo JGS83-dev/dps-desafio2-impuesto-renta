1d6cc8106ec9d750cb0bfaacdfa3c1315a43e19f
WilfredoAcosta <wilfredo.acosta10@gmail.com>
2023-10-06 13:32:24 -0600

    Diseño de 2 completado

--- components/Formulario.js
+++ components/Formulario.js
@@ -2,17 +2,66 @@ import React from 'react';
 import ContenedorPrincipal from './ContenedorPrincipal';
 import {
     Text,
-    StyleSheet
+    StyleSheet,
+    Dimensions,
+    View,
+    TextInput,
+    TouchableOpacity
 } from 'react-native';
 import { colores } from '../config/colores';
-
+const screenHeight = Dimensions.get("window").height;
+const cabeceraMensajeHeight = 0.07 * screenHeight;
+const cabeceraMensajePadding = 0.01 * screenHeight;
+const cabeceraMensajeMargin = 0.03 * screenHeight;
 const Formulario = ({navigation}) => {
     return (
         
             <ContenedorPrincipal titulo="Formulario"
             navigation={navigation} 
             contenido={(
-                <Text style={styles.letra}>Formulario</Text>
+                <>
+            <View style={styles.container}>
+                        <Text style={styles.letraTitulo}>Complete los formularios</Text>
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.letra}>Nombre:</Text>
+                            <TextInput
+                                placeholder="Nombres"
+                            
+                                style={styles.input}
+                                required
+                            />
+                        </View>
+
+
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.letra}>Apellido:</Text>
+                            <TextInput
+                                placeholder="Apellido"
+                              
+                                style={styles.input}
+                                required
+                            />
+                        </View>
+
+                        <View style={styles.inputContainer}>
+                            <Text style={styles.letra}>Sueldo:</Text>
+                            <TextInput
+                                placeholder="Sueldo"
+                            
+                                style={styles.input}
+                                required
+                            />
+                        </View>
+
+                        <View style={styles.buttonContainer}>
+                            <TouchableOpacity style={styles.button}>
+                                <Text style={styles.buttonText}>Ver calculo de salario</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+
+                </>
+               
             )}></ContenedorPrincipal>
         
     );
@@ -24,7 +73,60 @@ const styles = StyleSheet.create({
     letra: {
         fontSize: 20,
         fontWeight: 'bold',
-        color: colores.letra
-    }
+        color: colores.letra,
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    containerIzquierdo: {
+        justifyContent: "flex-start",
+        padding: 5,
+    },
+    inputContainer: {
+        width: "80%",
+        flexDirection: "row",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+        justifyContent: "space-evenly",
+    },
+    buttonContainer: {
+        width: "60%",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 20,
+    },
+    button: {
+        backgroundColor: colores.fondoBarras,
+        width: "100%",
+        padding: 5,
+        alignItems: "center",
+        marginBottom: 10,
+    },
+    buttonText: {
+        color: colores.letra,
+        fontSize: 16,
+    },
+    letraTitulo: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: colores.letra,
+    },
+    letra: {
+        fontSize: 16,
+        color: colores.letra,
+    },
+    cabeceraMensaje: {
+        backgroundColor: colores.fondoBarras,
+        height: cabeceraMensajeHeight,
+        padding: cabeceraMensajePadding,
+        marginTop: cabeceraMensajeMargin,
+        alignItems: "center",
+        alignContent: "center",
+    },
 });
 
--- components/Resultados.js
+++ components/Resultados.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ContenedorPrincipal from './ContenedorPrincipal';
 import {
     Text,
-    StyleSheet
+    StyleSheet,
+    View
 } from 'react-native';
 import { colores } from '../config/colores';
 
@@ -12,7 +13,30 @@ const Resultados = ({ navigation }) => {
         <ContenedorPrincipal titulo="Resultados"
             navigation={navigation}
             contenido={(
-                <Text style={styles.letra}>Resultados</Text>
+               <>
+               <View style={styles.container}>
+                                <View style={styles.inputContainer}>
+                                <Text style={styles.letra}>Desc. Renta:</Text>
+                                    <Text style={styles.desc}>Descuento</Text>
+                                </View>
+
+                                <View style={styles.inputContainer}>
+                                <Text style={styles.letra}>Desc. ISSS:</Text>
+                                    <Text style={styles.desc}>Descuento</Text>
+                                </View>
+
+                                <View style={styles.inputContainer}>
+                                <Text style={styles.letra}>Desc. AFP:</Text>
+                                    <Text style={styles.desc}>Descuento</Text>
+                             
+                                </View>
+
+                                <View style={styles.inputContainer}>
+                                    <Text style={styles.letra}>Sueldo neto:</Text>
+                                    <Text style={styles.desc}>Descuento</Text>
+                                </View>
+                            </View>
+               </>
             )}></ContenedorPrincipal>
 
     );
@@ -22,9 +46,28 @@ export default Resultados
 
 const styles = StyleSheet.create({
     letra: {
-        fontSize: 20,
+        fontSize: 24,
         fontWeight: 'bold',
-        color: colores.letra
-    }
+        color: '#171D26',
+        textAlign:'center',
+    },
+    desc: {
+        fontSize: 20,
+        color: '#F2F4F7'
+    },
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    inputContainer: {
+        width: "80%",
+        flexDirection: "row",
+        paddingHorizontal: 15,
+        paddingVertical: 10,
+        borderRadius: 10,
+        marginTop: 5,
+        justifyContent: "space-evenly",
+    },
 });
 
