c9342c265e32103dfe73849f2755153bbafec9cd
birdkron <js8215001@gmail.com>
2023-10-08 08:42:48 -0600

    Completado toma de datos

--- components/Formulario.js
+++ components/Formulario.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContenedorPrincipal from './ContenedorPrincipal';
 import {
     Text,
@@ -13,20 +13,48 @@ const screenHeight = Dimensions.get("window").height;
 const cabeceraMensajeHeight = 0.07 * screenHeight;
 const cabeceraMensajePadding = 0.01 * screenHeight;
 const cabeceraMensajeMargin = 0.03 * screenHeight;
-const Formulario = ({navigation}) => {
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const Formulario = ({ navigation }) => {
+
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [sueldo, setSueldo] = useState(0);
+
+    const GuardarDatosFormulario = async () => {
+        try {
+            await AsyncStorage.setItem('nombre', nombre);
+            await AsyncStorage.setItem('apellido', apellido);
+            await AsyncStorage.setItem('sueldo', sueldo.toString());
+            let renta = 0;
+            if (sueldo > 2500) {
+                renta = sueldo * (25 / 100);
+            } else if (sueldo >= 1000 && sueldo <= 2500) {
+                renta = sueldo * (18 / 100);
+            } else {
+                renta = sueldo * (7 / 100);
+            }
+            renta = renta.toFixed(2);
+            await AsyncStorage.setItem('renta', renta.toString());
+            navigation.navigate("Resultados");
+        } catch (e) {
+            console.log('Ocurrio un error:', e);
+        }
+    };
+
     return (
-        
-            <ContenedorPrincipal titulo="Formulario"
-            navigation={navigation} 
+
+        <ContenedorPrincipal titulo="Formulario"
+            navigation={navigation}
             contenido={(
                 <>
-            <View style={styles.container}>
+                    <View style={styles.container}>
                         <Text style={styles.letraTitulo}>Complete los formularios</Text>
                         <View style={styles.inputContainer}>
                             <Text style={styles.letra}>Nombre:</Text>
                             <TextInput
                                 placeholder="Nombres"
-                            
+                                onChangeText={text => setNombre(text)}
                                 style={styles.input}
                                 required
                             />
@@ -37,7 +65,7 @@ const Formulario = ({navigation}) => {
                             <Text style={styles.letra}>Apellido:</Text>
                             <TextInput
                                 placeholder="Apellido"
-                              
+                                onChangeText={text => setApellido(text)}
                                 style={styles.input}
                                 required
                             />
@@ -47,7 +75,7 @@ const Formulario = ({navigation}) => {
                             <Text style={styles.letra}>Sueldo:</Text>
                             <TextInput
                                 placeholder="Sueldo"
-                            
+                                onChangeText={text => setSueldo(text)}
                                 style={styles.input}
                                 required
                                 keyboardType="numeric"
@@ -55,16 +83,17 @@ const Formulario = ({navigation}) => {
                         </View>
 
                         <View style={styles.buttonContainer}>
-                            <TouchableOpacity style={styles.button}>
+                            <TouchableOpacity style={styles.button}
+                                onPress={GuardarDatosFormulario}>
                                 <Text style={styles.buttonText}>Ver calculo de salario</Text>
                             </TouchableOpacity>
                         </View>
                     </View>
 
                 </>
-               
+
             )}></ContenedorPrincipal>
-        
+
     );
 }
 
