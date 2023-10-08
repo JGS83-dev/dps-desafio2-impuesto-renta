07f4155e6d1178d58d949397af70529ac2912699
birdkron <js8215001@gmail.com>
2023-10-08 08:43:16 -0600

    Completado Vista de Resultado de calculos

--- components/Resultados.js
+++ components/Resultados.js
@@ -1,42 +1,105 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ContenedorPrincipal from './ContenedorPrincipal';
 import {
     Text,
     StyleSheet,
-    View
+    View,
+    ScrollView
 } from 'react-native';
 import { colores } from '../config/colores';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const Resultados = ({ navigation }) => {
+
+    const [nombre, setNombre] = useState('');
+    const [apellido, setApellido] = useState('');
+    const [sueldo, setSueldo] = useState(0);
+    const [renta, setRenta] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const CargarDatosFormulario = async () => {
+            try {
+                console.log("Leyendo datos...");
+                const nombreStorage = await AsyncStorage.getItem('nombre');
+                const apellidoStorage = await AsyncStorage.getItem('apellido');
+                const sueldoStorage = await AsyncStorage.getItem('sueldo');
+                const rentaStorage = await AsyncStorage.getItem('renta');
+
+                if (nombreStorage !== null && apellidoStorage !== null && sueldoStorage !== 0) {
+                    setNombre(nombreStorage);
+                    setApellido(apellidoStorage);
+                    setSueldo(sueldoStorage);
+                    setRenta(rentaStorage);
+                } else {
+                    Alert.alert(
+                        'Pasos requeridos',
+                        'Debe completar el formulario',
+                        [
+                            {
+                                text: 'Aceptar',
+                                onPress: () => {
+                                    console.log('Regresando a formulario');
+                                    navigation.navigate("Formulario");
+                                },
+                                style: 'default',
+                            },
+                        ],
+                    );
+                }
+
+            } catch (e) {
+                console.log('Ocurrio un error:', e);
+            }
+        };
+        CargarDatosFormulario();
+    }, []);
+
+    useEffect(() => {
+        if (nombre.length > 0) {
+            console.log('Data cargada');
+            setIsLoading(false);
+        }
+    }, [nombre, apellido, sueldo, renta])
+
     return (
 
-        <ContenedorPrincipal titulo="Resultados"
+        <ContenedorPrincipal titulo="Calculo del ISR"
             navigation={navigation}
             contenido={(
-               <>
-               <View style={styles.container}>
-                                <View style={styles.inputContainer}>
-                                <Text style={styles.letra}>Desc. Renta:</Text>
-                                    <Text style={styles.desc}>Descuento</Text>
-                                </View>
+                <>
+                    {isLoading ? (<Text style={styles.letra}>Calculando...</Text>) :
+                        (
+                                <View style={styles.container}>
+                                <Text style={styles.letraTitulo}>Resultado</Text>
+                                    <View style={styles.inputContainer}>
+                                        <Text style={styles.letra}>Nombre:</Text>
+                                        <Text style={styles.desc}>{nombre}</Text>
+                                    </View>
 
-                                <View style={styles.inputContainer}>
-                                <Text style={styles.letra}>Desc. ISSS:</Text>
-                                    <Text style={styles.desc}>Descuento</Text>
-                                </View>
+                                    <View style={styles.inputContainer}>
+                                        <Text style={styles.letra}>Apellidos:</Text>
+                                        <Text style={styles.desc}>{apellido}</Text>
+                                    </View>
 
-                                <View style={styles.inputContainer}>
-                                <Text style={styles.letra}>Desc. AFP:</Text>
-                                    <Text style={styles.desc}>Descuento</Text>
-                             
-                                </View>
+                                    <View style={styles.inputContainer}>
+                                        <Text style={styles.letra}>Sueldo:</Text>
+                                        <Text style={styles.desc}>${sueldo}</Text>
+                                    </View>
 
-                                <View style={styles.inputContainer}>
-                                    <Text style={styles.letra}>Sueldo neto:</Text>
-                                    <Text style={styles.desc}>Descuento</Text>
+                                    <View style={styles.inputContainer}>
+                                        <Text style={styles.letra}>Desc. Renta:</Text>
+                                        <Text style={styles.desc}>${renta}</Text>
+                                    </View>
+
+                                    <View style={styles.inputContainer}>
+                                        <Text style={styles.letra}>Sueldo neto:</Text>
+                                        <Text style={styles.desc}>${sueldo - renta}</Text>
+                                    </View>
                                 </View>
-                            </View>
-               </>
+                        )
+                    }
+                </>
             )}></ContenedorPrincipal>
 
     );
@@ -45,15 +108,23 @@ const Resultados = ({ navigation }) => {
 export default Resultados
 
 const styles = StyleSheet.create({
+    letraTitulo: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: colores.letra,
+        textAlign: 'center',
+        padding: 5,
+        marginBottom: 5,
+    },
     letra: {
         fontSize: 24,
         fontWeight: 'bold',
-        color: '#171D26',
-        textAlign:'center',
+        color: colores.primario,
+        textAlign: 'center',
     },
     desc: {
         fontSize: 20,
-        color: '#F2F4F7'
+        color: colores.secundario,
     },
     container: {
         flex: 1,
