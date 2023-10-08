31e5018bbe1d09ae57c98d24e65e4e886d11e422
birdkron <js8215001@gmail.com>
2023-10-08 08:42:16 -0600

    Ajuste en paleta de colores

--- config/colores.js
+++ config/colores.js
@@ -1,8 +1,8 @@
 export const colores = {
     fondo: '#313E50',
     fondoBarras: '#6C6F7F',
-    primario: '#455561',
-    secundario: '#5C6672',
+    primario: '#171D26',
+    secundario: '#F2F4F7',
     terciario: '#6C6F7F',
     letra: '#FFFFFF',
 }
